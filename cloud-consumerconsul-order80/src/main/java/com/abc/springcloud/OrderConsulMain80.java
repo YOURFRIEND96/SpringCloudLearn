53edf68e8b0e80e81cc52fc692dddcf76ea635a2
xiaolosh <xlshen@cisco.com>
2021-06-08 17:42:07 +0800

    zookeeper and consul

--- cloud-consumerconsul-order80/src/main/java/com/abc/springcloud/OrderConsulMain80.java
+++ cloud-consumerconsul-order80/src/main/java/com/abc/springcloud/OrderConsulMain80.java
@@ -3,15 +3,13 @@ package com.abc.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 @EnableDiscoveryClient
-public class PaymentMain8001 {
-    
+public class OrderConsulMain80 {
+
     public static void main(String[] args) {
-        SpringApplication.run(PaymentMain8001.class, args);
+        SpringApplication.run(OrderConsulMain80.class, args);
     }
 
 }
--- cloud-provider-payment8001/src/main/java/com/abc/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/abc/springcloud/controller/PaymentController.java
@@ -5,9 +5,12 @@ import entities.CommonResult;
 import entities.Payment;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -17,7 +20,26 @@ public class PaymentController {
     private String serverPort;
 
     @Resource
+    private DiscoveryClient discoveryClient;
+
+    @Resource
     private PaymentService paymentService;
+
+    @GetMapping(value = "/payment/discovery")
+    public Object discovery() {
+
+        List<String> services = discoveryClient.getServices();
+        for (String element : services) {
+            log.info("****element: "+element);
+        }
+        List<ServiceInstance> instances = discoveryClient.getInstances("cloud-payment-service");
+        for (ServiceInstance instance : instances) {
+            log.info(instance.getServiceId()+"\t"+instance.getHost()+"\t"+instance.getPort()+"\t"+instance.getUri());
+        }
+
+        return this.discoveryClient;
+    }
+
 
     @PostMapping(value = "/payment/create")
     public CommonResult create(@RequestBody Payment payment)
